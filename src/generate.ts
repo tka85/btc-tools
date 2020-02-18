2cf68dfdf5acd5e6e3e884f6c1858529b087379a
tak85 <thaliskalf@gmail.com>
2020-02-18 15:09:15 +0100

    Adds generate script tests
    Plus minor fixes in variable naming and in the package's
    build script (lint)

--- src/generate.ts
+++ src/generate.ts
@@ -13,7 +13,10 @@ const LANGS = {
     'ko': 'korean'
 };
 
-export function generateMnemonic(lang: 'en' | 'sp' | 'fr' | 'it' | 'ja' | 'ko' = 'en', printStdOut: boolean = false): string {
+/**
+ * @return {string} mnemonic    a random mnemonic in specified language
+ */
+export function generateMnemonic(lang: 'en' | 'es' | 'fr' | 'it' | 'jp' | 'ko' = 'en', printStdOut: boolean = false): string {
     const language = LANGS[lang];
     shuffle(bip39.wordlists[language]);
     const mnemonic = bip39.wordlists[language].slice(0, 24).join(' ');
@@ -23,6 +26,9 @@ export function generateMnemonic(lang: 'en' | 'sp' | 'fr' | 'it' | 'ja' | 'ko' =
     return mnemonic;
 }
 
+/**
+ * @return {string} seed    a random seed in hex
+ */
 export function generateSeed(printStdOut: boolean = false): string {
     const mnemonic = generateMnemonic('en');
     const seed = bip39.mnemonicToSeedSync(mnemonic).toString('hex');
@@ -32,6 +38,9 @@ export function generateSeed(printStdOut: boolean = false): string {
     return seed;
 }
 
+/**
+ * @return {string} extKey    a random ext key in specific format
+ */
 export function generateExtKey(extKeyType: 'xprv' | 'xpub' | 'yprv' | 'ypub' | 'Yprv' | 'Ypub' | 'zprv' | 'zpub' | 'Zprv' | 'Zpub' | 'tprv' | 'tpub' | 'uprv' | 'upub' | 'Uprv' | 'Upub' | 'vprv' | 'vpub' | 'Vprv' | 'Vpub' = 'xpub', printStdOut: boolean = false): string {
     const network = ['xprv', 'xpub', 'yprv', 'ypub', 'Yprv', 'Ypub', 'zprv', 'zpub', 'Zprv', 'Zpub'].includes(extKeyType) ? bitcoinjs.networks.bitcoin : bitcoinjs.networks.testnet;
     const rootNode = generateRoot(network);
@@ -74,6 +83,9 @@ export function generateExtKey(extKeyType: 'xprv' | 'xpub' | 'yprv' | 'ypub' | '
     return extKey;
 }
 
+/**
+ * @return {bip32.BIP32Interface} rootNode    a random bip32 HD root node
+ */
 function generateRoot(network: bitcoinjs.networks.Network = bitcoinjs.networks.bitcoin): bip32.BIP32Interface {
     const seed = generateSeed();
     return bip32.fromSeed(Buffer.from(seed, 'hex'), network);
@@ -82,7 +94,7 @@ function generateRoot(network: bitcoinjs.networks.Network = bitcoinjs.networks.b
 // Fisher-Yates (in-place) shuffle
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
+        const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
@@ -91,17 +103,14 @@ if (require.main === module) {
     // used on command line
     program.option('-s, --seed', 'generate a random seed')
         .option('-m, --mnemonic <language>', 'generate a random mnemonic 24 words long from bip39 wordlist; language can be one of "english","spanish","french","italian","japanese","korean"')
-        .option('-p, --ext-prv <keyFormat>', 'generate a random ext prv key; key format can be "xprv" | "yprv" | "Yprv" | "zprv" | "Zprv" | "tprv" | "uprv" | "Uprv" | "vprv" | "Vprv"')
-        .option('-P, --ext-pub <keyFormat>', 'generate a random ext pub key; key format can be "xpub" | "ypub" | "Ypub" | "zpub" | "Zpub" | "tpub" | "upub" | "Upub" | "vpub" | "Vpub"')
+        .option('-e, --ext-key <keyFormat>', 'generate a random ext prv or pub key; key format can be "xprv" | "yprv" | "Yprv" | "zprv" | "Zprv" | "tprv" | "uprv" | "Uprv" | "vprv" | "Vprv" | "xpub" | "ypub" | "Ypub" | "zpub" | "Zpub" | "tpub" | "upub" | "Upub" | "vpub" | "Vpub"');
 
     program.parse(process.argv);
     if (program.seed) {
         generateSeed(true);
     } else if (program.mnemonic) {
         generateMnemonic(program.mnemonic, true);
-    } else if (program.extPrv) {
-        generateExtKey(program.extPrv, true);
-    } else if (program.extPub) {
-        generateExtKey(program.extPub, true);
+    } else if (program.extKey) {
+        generateExtKey(program.extKey, true);
     }
 }
