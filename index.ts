18dafcdcc17c27b1f2bb6ca55fb6f1a9e9d49c5d
tak85 <thaliskalf@gmail.com>
2020-02-19 14:29:41 +0100

    Adds DerivationPath class
    DerivationPath class models a bip32 path and supports operations:
    add one level down, go one level up, increment.
    Extracts common methods into lib/utils
    Cmd line param for extensible keys is changed from -e to -x
    for both derive and generate scripts.

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
 export * from './src/derive';
 export * from './src/convertXpub';
+export * from './src/generate';
+export * from './src/lib/DerivationPath';
--- src/derive.spec.ts
+++ src/derive.spec.ts
@@ -3,10 +3,11 @@ import { throws, deepEqual } from 'assert';
 import bitcoinjs = require('bitcoinjs-lib');
 
 describe('derive', () => {
-    const tpriv = 'tprv8ZgxMBicQKsPd7Uf69XL1XwhmjHopUGep8GuEiJDZmbQz6o58LninorQAfcKZWARbtRtfnLcJ5MQ2AtHcQJCCRUcMRvmDUjyEmNUWwx8UbK';
+    const tprv = 'tprv8ZgxMBicQKsPd7Uf69XL1XwhmjHopUGep8GuEiJDZmbQz6o58LninorQAfcKZWARbtRtfnLcJ5MQ2AtHcQJCCRUcMRvmDUjyEmNUWwx8UbK';
     const tpub = 'tpubD6NzVbkrYhZ4WaWSyoBvQwbpLkojyoTZPRsgXELWz3Popb3qkjcJyJUGLnL4qHHoQvao8ESaAstxYSnhyswJ76uZPStJRJCTKvosUCJZL5B';
-    const network = 'testnet';
     const path = 'm/0';
+    const hardenedPath0 = "0'";
+    const hardenedPath1 = '0h';
     const derived00 = [
         {
             bech32: 'tb1qm90ugl4d48jv8n6e5t9ln6t9zlpm5th690vysp',
@@ -14,6 +15,15 @@ describe('derive', () => {
             legacy: 'n1LKejAadN6hg2FrBXoU1KrwX4uK16mco9',
             p2sh_segwit: '2N2gQKzjUe47gM8p1JZxaAkTcoHPXV6YyVp',
             path: 'm/0/0'
+        }
+    ];
+    const derived0h = [
+        {
+            bech32: 'tb1qalzchqutx9f3wjln69nhkusnx5aymn8a5tyk9c',
+            depth: 2,
+            legacy: 'n3NkSZqoPMCQN5FENxUBw4qVATbytH6FDK',
+            p2sh_segwit: '2NA9LWMy8Bn5QTs5CB5Z8Fbqm66oDbp8KL4',
+            path: "m/0'/0"
         }
     ];
     const derived00CustomCols = [{ bech32: 'tb1qm90ugl4d48jv8n6e5t9ln6t9zlpm5th690vysp' }];
@@ -34,39 +44,36 @@ describe('derive', () => {
         }
     ];
     const invalidTpub = 'tpubD6NzVbkrYhZ4WaWSyoBvQwbpLkojyoTZPRsgXEL';
-    const invalidNetwork = 'foonet';
     const invalidPath = '///';
     it('should fail if missing key', () => {
-        throws(() => { derive({ key: undefined, network: 'testnet', path: '0/0' }) }, /missing extended key/);
-    });
-    it('should fail if missing network', () => {
-        throws(() => { derive({ key: tpub, network: undefined, path: '0/0' }) }, /missing network/);
+        throws(() => { derive({ key: undefined, path: '0/0' }) }, /missing extended key/);
     });
     it('should fail if missing path', () => {
-        throws(() => { derive({ key: tpub, network: 'testnet', path: undefined }) }, /missing or invalid path/);
+        throws(() => { derive({ key: tpub, path: undefined }) }, /missing or invalid path/);
     });
     it('should fail if key not of known type', () => {
-        throws(() => { derive({ key: 'UNKOWNtpub', network: 'testnet', path: '0/0' }) }, /Do not know how to convert ext key with prefix "UNKO"/);
-    });
-    it('should fail if invalid network', () => {
-        throws(() => { derive({ key: tpub, network: invalidNetwork, path: '0/0' }) }, /Invalid network specified: "foonet"; expected "mainnet" or "testnet"/);
+        throws(() => { derive({ key: 'UNKOWNtpub', path: '0/0' }) }, /Do not know how to convert ext key with prefix "UNKO"/);
     });
     it('should fail if key of known type but invalid key', () => {
-        throws(() => { derive({ key: invalidTpub, network: 'testnet', path: '0/0' }) }, /Invalid extended public keyError: Invalid checksum/);
+        throws(() => { derive({ key: invalidTpub, path: '0/0' }) }, /Invalid extended public keyError: Invalid checksum/);
     });
     it('should fail if invalid path', () => {
-        throws(() => { derive({ key: tpub, network: 'testnet', path: invalidPath }) }, /Expected BIP32Path, got String ".*"/);
+        throws(() => { derive({ key: tpub, path: invalidPath }) }, /Expected BIP32Path, got String ".*"/);
     });
     it('should derive correctly from tprv', () => {
-        deepEqual(derive({ key: tpriv, network, path, count: 1 }), derived00);
+        deepEqual(derive({ key: tprv, path, count: 1 }), derived00);
     });
     it('should derive correctly from tpub', () => {
-        deepEqual(derive({ key: tpub, network, path, count: 1 }), derived00);
+        deepEqual(derive({ key: tpub, path, count: 1 }), derived00);
     });
     it('should derive correctly with custom columns', () => {
-        deepEqual(derive({ key: tpub, network, path, cols: 'bech32', count: 1 }), derived00CustomCols);
+        deepEqual(derive({ key: tpub, path, cols: 'bech32', count: 1 }), derived00CustomCols);
     });
     it('should derive correctly including root', () => {
-        deepEqual(derive({ key: tpub, network, path, includeRoot: true, count: 1 }), derived00WithRoot);
+        deepEqual(derive({ key: tpub, path, includeRoot: true, count: 1 }), derived00WithRoot);
+    });
+    it('should derive correctly hardened path (both formats)', () => {
+        deepEqual(derive({ key: tprv, path: hardenedPath0, includeRoot: false, count: 1 }), derived0h);
+        deepEqual(derive({ key: tprv, path: hardenedPath1, includeRoot: false, count: 1 }), derived0h);
     });
 });
