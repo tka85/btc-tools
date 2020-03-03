3c77b44cf44c165983941ef1bf761316fcb09fd3
tak85 <thaliskalf@gmail.com>
2020-03-04 00:37:14 +0100

    Improves derive script
    Adds wif, renames cols and aliases.

--- src/derive.spec.ts
+++ src/derive.spec.ts
@@ -7,48 +7,69 @@ describe('derive', () => {
     const path = 'm/0';
     const hardenedPath0 = "0'";
     const hardenedPath1 = '0h';
-    const derivedNonHardened = [
+    const derivedNonHardenedNotNeutered = [
         {
-            bech32: 'tb1qm90ugl4d48jv8n6e5t9ln6t9zlpm5th690vysp',
+            p2wpkh: 'tb1qm90ugl4d48jv8n6e5t9ln6t9zlpm5th690vysp',
             depth: 2,
-            legacy: 'n1LKejAadN6hg2FrBXoU1KrwX4uK16mco9',
-            p2sh_segwit: '2N2gQKzjUe47gM8p1JZxaAkTcoHPXV6YyVp',
-            path: 'm/0/0'
+            p2pkh: 'n1LKejAadN6hg2FrBXoU1KrwX4uK16mco9',
+            p2sh_p2wpkh: '2N2gQKzjUe47gM8p1JZxaAkTcoHPXV6YyVp',
+            path: 'm/0/0',
+            pubkey: '02c97dc3f4420402e01a113984311bf4a1b8de376cac0bdcfaf1b3ac81f13433c7',
+            wif: 'cTiN5q13XR9ebmqfXbaCbz2x6C6inEJWqoJ5jP4CCw98ZCJ4gkuN'
+        }
+    ];
+    const derivedNonHardenedNeutered = [
+        {
+            p2wpkh: 'tb1qm90ugl4d48jv8n6e5t9ln6t9zlpm5th690vysp',
+            depth: 2,
+            p2pkh: 'n1LKejAadN6hg2FrBXoU1KrwX4uK16mco9',
+            p2sh_p2wpkh: '2N2gQKzjUe47gM8p1JZxaAkTcoHPXV6YyVp',
+            path: 'm/0/0',
+            pubkey: '02c97dc3f4420402e01a113984311bf4a1b8de376cac0bdcfaf1b3ac81f13433c7',
+            wif: null
         }
     ];
     const derivedHardened = [
         {
-            bech32: 'tb1qalzchqutx9f3wjln69nhkusnx5aymn8a5tyk9c',
+            p2wpkh: 'tb1qalzchqutx9f3wjln69nhkusnx5aymn8a5tyk9c',
             depth: 2,
-            legacy: 'n3NkSZqoPMCQN5FENxUBw4qVATbytH6FDK',
-            p2sh_segwit: '2NA9LWMy8Bn5QTs5CB5Z8Fbqm66oDbp8KL4',
-            path: "m/0'/0"
+            p2pkh: 'n3NkSZqoPMCQN5FENxUBw4qVATbytH6FDK',
+            p2sh_p2wpkh: '2NA9LWMy8Bn5QTs5CB5Z8Fbqm66oDbp8KL4',
+            path: "m/0'/0",
+            pubkey: '02fcba7ecf41bc7e1be4ee122d9d22e3333671eb0a3a87b5cdf099d59874e1940f',
+            wif: 'cNaQCDwmmh4dS9LzCgVtyy1e1xjCJ21GUDHe9K98nzb689JvinGV'
         }
     ];
     const derivedHardenedChild = [
         {
-            bech32: 'tb1q97fke3clxkzr5hr8hq7yadd7ljs3lchehh0kv0',
+            p2wpkh: 'tb1q97fke3clxkzr5hr8hq7yadd7ljs3lchehh0kv0',
             depth: 2,
-            legacy: 'mjrWfLF9MemTz6jL4yuWZ3qWuA9thoUZMe',
-            p2sh_segwit: '2N6aBpWwCaUovasCfWJmnNLVpBh6D9ChBUW',
-            path: "m/0/0'"
+            p2pkh: 'mjrWfLF9MemTz6jL4yuWZ3qWuA9thoUZMe',
+            p2sh_p2wpkh: '2N6aBpWwCaUovasCfWJmnNLVpBh6D9ChBUW',
+            path: "m/0/0'",
+            pubkey: '028b286667010c16638ef5769831efe2cbe0c72b8555e56692694d3b0900704b57',
+            wif: 'cQJ9KFJkWqMfYbQpXvVQKCMHBwMorvtR81JZsiYP9dFWMe2sP1Hc'
         }
     ];
-    const derived00CustomCols = [{ bech32: 'tb1qm90ugl4d48jv8n6e5t9ln6t9zlpm5th690vysp' }];
+    const derived00CustomCols = [{ p2wpkh: 'tb1qm90ugl4d48jv8n6e5t9ln6t9zlpm5th690vysp' }];
     const derived00WithRoot = [
         {
-            bech32: "tb1qp30e58hrp0etgsl2q9y4tar26a93nwc0wa0zh4",
+            p2wpkh: 'tb1qp30e58hrp0etgsl2q9y4tar26a93nwc0wa0zh4',
             depth: 0,
-            legacy: "mgeNvuFj7FD6MzAYNzdUB7HpUWezFjvuud",
-            p2sh_segwit: "2MwGQiqSYMXwzzgorZi5V4oBJpC7AA6jsPY",
-            path: "m/"
+            p2pkh: 'mgeNvuFj7FD6MzAYNzdUB7HpUWezFjvuud',
+            p2sh_p2wpkh: '2MwGQiqSYMXwzzgorZi5V4oBJpC7AA6jsPY',
+            path: 'm/',
+            pubkey: '023e4740d0ba639e28963f3476157b7cf2fb7c6fdf4254f97099cf8670b505ea59',
+            wif: null
         },
         {
-            bech32: "tb1qm90ugl4d48jv8n6e5t9ln6t9zlpm5th690vysp",
+            p2wpkh: 'tb1qm90ugl4d48jv8n6e5t9ln6t9zlpm5th690vysp',
             depth: 2,
-            legacy: "n1LKejAadN6hg2FrBXoU1KrwX4uK16mco9",
-            p2sh_segwit: "2N2gQKzjUe47gM8p1JZxaAkTcoHPXV6YyVp",
-            path: "m/0/0"
+            p2pkh: 'n1LKejAadN6hg2FrBXoU1KrwX4uK16mco9',
+            p2sh_p2wpkh: '2N2gQKzjUe47gM8p1JZxaAkTcoHPXV6YyVp',
+            path: 'm/0/0',
+            pubkey: '02c97dc3f4420402e01a113984311bf4a1b8de376cac0bdcfaf1b3ac81f13433c7',
+            wif: null
         }
     ];
     const invalidChecksumTpub = 'tpubD6NzVbkrYhZ4WaWSyoBvQwbpLkojyoTZPRsgXEL';
@@ -66,13 +87,13 @@ describe('derive', () => {
         throws(() => { derive({ extKey: tpub, path: invalidPath, hardenedChildren: false, }) }, /Expected BIP32Path, got String ".*"/);
     });
     it('should derive correctly from tprv', () => {
-        deepEqual(derive({ extKey: tprv, path, hardenedChildren: false, count: 1 }), derivedNonHardened);
+        deepEqual(derive({ extKey: tprv, path, hardenedChildren: false, count: 1 }), derivedNonHardenedNotNeutered);
     });
     it('should derive correctly from tpub', () => {
-        deepEqual(derive({ extKey: tpub, path, hardenedChildren: false, count: 1 }), derivedNonHardened);
+        deepEqual(derive({ extKey: tpub, path, hardenedChildren: false, count: 1 }), derivedNonHardenedNeutered);
     });
     it('should derive correctly with custom columns', () => {
-        deepEqual(derive({ extKey: tpub, path, cols: 'bech32', hardenedChildren: false, count: 1 }), derived00CustomCols);
+        deepEqual(derive({ extKey: tpub, path, cols: 'p2wpkh', hardenedChildren: false, count: 1 }), derived00CustomCols);
     });
     it('should derive correctly including root', () => {
         deepEqual(derive({ extKey: tpub, path, hardenedChildren: false, includeRoot: true, count: 1 }), derived00WithRoot);
